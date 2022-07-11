e5ce7d1b00348e2cd87353a06473a0c593761918
André Klunk <andreklunk@yahoo.com.br>
2022-07-11 17:01:51 -0300

    Update conexao.php

--- intranet/conexao.php
+++ intranet/conexao.php
@@ -1,31 +1,13 @@
 <?php	
-/*
-	$DB_HOST = 'localhost';
-    $DB_NAME = 'id1301142_sisuni_md';
-    $DB_USER = 'id1301142_sisuni_md';
-    $DB_PASS = 'mondai2017';
-*/
-	$DB_HOST = 'localhost';
-    $DB_NAME = 'sisuni_sjo';
-    $DB_USER = 'root';
-    $DB_PASS = 'maria';
-/*	$conexao = mysql_connect($DB_HOST,$DB_USER,$DB_PASS);
-	if(!($conexao))
-    {
-        echo "Não foi possível estabelecer uma conexão com o gerenciador MySQL.
-		 Favor Contactar o Administrador.";
-        exit;
-    }
-	
-	$banco = mysql_select_db($DB_NAME,$conexao);
-    if(!($banco))
-    {
-        echo "Não foi possível encontrar o Banco de Dados. 
-		Favor Contactar o Administrador.";
-        exit;
-    }
-	mysql_set_charset('UTF8', $conexao);	*/
-	//PARÂMETROS PARA MySqli
-	$link = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME) or die("Erro " . mysqli_error($link));
-	mysqli_set_charset($link, "utf8");
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+
+mysqli_set_charset($link, "utf8");
 ?>
