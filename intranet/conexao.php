3cca364c52694b132a0fc60c1d4a959aafe464b8
andreklunk <andreklunk@yahoo.com.br>
2022-07-11 18:07:20 -0300

    Update conexao.php

--- intranet/conexao.php
+++ intranet/conexao.php
@@ -5,7 +5,9 @@ $server = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
-
+echo $server; 
+echo "<br>";
+echo $db;
 $conn = new mysqli($server, $username, $password, $db);
 
 
