8f573a26f687e1f0962f2b31fe0196a5dc433e71
André Klunk <andreklunk@yahoo.com.br>
2022-07-11 17:11:39 -0300

    Update conexao.php

--- intranet/conexao.php
+++ intranet/conexao.php
@@ -1,13 +1,14 @@
 <?php	
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
+echo "ola";
+echo "ola";
 $server = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
-echo "ola";
+
 
 mysqli_set_charset($link, "utf8");
 ?>
